24e5eae4572e908147d97507feab1b8ef423969f
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-09-24 21:50:20 +0700

    beej: array

--- src/beej/section-6.c
+++ src/beej/section-6.c
@@ -2,6 +2,38 @@
 
 #define COUNT 5
 
+void times2(int *a, int len)
+{
+    printf("----times2-----\n");
+    for (int i = 0; i < len; i++)
+        printf("%d\n", a[i] * 2);
+    printf("---------\n");
+}
+void times3(int a[], int len)
+{
+    printf("----times3-----\n");
+    for (int i = 0; i < len; i++)
+        printf("%d\n", a[i] * 3);
+    printf("---------\n");
+}
+void times4(int a[5], int len)
+{
+    printf("----times4-----\n");
+    for (int i = 0; i < len; i++)
+        printf("%d\n", a[i] * 4);
+    printf("---------\n");
+}
+
+void print_2D_array(int a[][3])
+{
+    for (int row = 0; row < 2; row++)
+    {
+        for (int col = 0; col < 3; col++)
+            printf("%d ", a[row][col]);
+        printf("\n");
+    }
+}
+
 void main(void)
 {
     int intArr1[5];
@@ -24,10 +56,24 @@ void main(void)
     int lengthOfString = sizeof characters;
     // printf("length of string: %d\n", lengthOfString); // 5
 
-    int intArr3[COUNT] = {[COUNT - 3] = 3, 2, 1};
+    int intArr3[COUNT] = {6, [COUNT - 3] = 3, 2, 1};
     int lengthOfIntArr3 = sizeof(intArr3) / sizeof(intArr3[0]);
-    for (int i = 0; i < lengthOfIntArr3; i++)
-    {
-        printf("%d: %d\n", i, intArr3[i]);
-    }
+    // for (int i = 0; i < lengthOfIntArr3; i++)
+    // {
+    //     printf("%d: %d\n", i, intArr3[i]);
+    // }
+
+    int *pointerOfIntArr3 = intArr3;
+    // printf("address: %d | value: %d\n", pointerOfIntArr3, *pointerOfIntArr3);
+
+    int x[5] = {11, 22, 33, 44, 55};
+    // times2(x, 5);
+    // times3(x, 5);
+    // times4(x, 5);
+
+    int y[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}};
+
+    print_2D_array(y);
 }
